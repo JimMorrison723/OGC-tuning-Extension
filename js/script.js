55dc3e518fb51f7baae1b5dfac28671c00f3f3b8
Balazs Szilagyi <bali723@gmail.com>
2011-09-03 20:51:10 +0200

    Added FormattingButtons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function smiley_gombok() {
+function SmileyButtons() {
         var arr = [ "heart", "minishock", "biggrin", "blushing", "bored", "mellow", "tongue", "ohmy", "blink", "smile", "sad", "crying", "wink", "xd" ];
         for (var i = 0; i < arr.length-1; i++) {
                 $('#hozzaszolas').before('<button id="smiley" value="'+arr[i]+'"><img src="'+chrome.extension.getURL('/img/smiley/'+arr[i]+'.png')+'" /></button>'); //value=":'+arr[i]+':" width=16 height=16
@@ -8,32 +8,53 @@ function smiley_gombok() {
         });    
  }
  
-function extInit() {
+function FormattingButtons() {
+	$('#hozzaszolas').before('<br ><button id="formbold" value="b">[B]</button>');
+	$('#hozzaszolas').before('<button id="formunder" value="u">[U]</button>');
+	$('#hozzaszolas').before('<button id="formitalics" value="i">[I]</button>');
+		
+	$(':button[id^="form"]').click(function () {
+        /* var textarea = jQuery('#message'); */
+        $('#message').val($('#message').val() + '<' + $(this).val() + '>' + getSelectedText() + '<' + $(this).val() + '>');
+        evt.preventDefault();
+    });  
+	
+	 function getSelectedText() {
+		if (window.getSelection) {
+			return window.getSelection();
+		}
+		else if (document.selection) {
+			return document.selection.createRange().text;	
+		}
+		return '';
+	}
+} 
  
-        if(dataStore['smiley_gombok'] == 'checked') {
-                smiley_gombok();
+
+	
+function extInit() {
+        if(dataStore['smiley_bar'] == 'checked') {
+                SmileyButtons();
         }
+ 		if(dataStore['formatting_toolbar'] == 'checked') {
+                FormattingButtons();
+        } 
 }
  
- 
 var dataStore;
- 
 // Create a port to BG process
 var port = chrome.extension.connect();
- 
 // Filter out iframes
 // Send "getSettings" request
 if (window.top === window) {
-                port.postMessage({ name : "getSettings" });
+    port.postMessage({ name : "getSettings" });
 }
  
 // Catch incoming messages
 port.onMessage.addListener(function(event) {
-       
         // Here comes the settings, save a copy to dataStore var
         if(event.name == 'setSettings') {
                 dataStore = event.message;
-                       
                 $(document).ready(function() {
                         extInit();
                 });
