faa826cb2e230a317cd7d183067f05d9cf154248
Balazs Szilagyi <bali723@gmail.com>
2011-09-03 17:44:31 +0200

    Javitas

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
-function smiley_gombok() { 
-	var arr = [ "heart", "minishock", "biggrin", "blushing", "bored", "mellow", "tongue", "ohmy", "blink", "smile", "sad", "crying", "wink", "xd" ];
-	for (var i = 0; i < arr.length-1; i++) {
-		$('#hozzaszolas').before('<button id="smiley" value="'+arr[i]+'"><img src="'+chrome.extension.getURL('/img/smiley/'+arr[i]+'.png')+'" /></button>'); //value=":'+arr[i]+':" width=16 height=16
-	}
-	$(":button[id=|smiley]").click(function () { 
-		wherever_cursor_is = $('#message').val($('#message').val()+":"+$(this).val()+":");
-	});	
- } 
-
-
-
-$(document).ready(function() {
-	var dataStore;
+function smiley_gombok() {
+        var arr = [ "heart", "minishock", "biggrin", "blushing", "bored", "mellow", "tongue", "ohmy", "blink", "smile", "sad", "crying", "wink", "xd" ];
+        for (var i = 0; i < arr.length-1; i++) {
+                $('#hozzaszolas').before('<button id="smiley" value="'+arr[i]+'"><img src="'+chrome.extension.getURL('/img/smiley/'+arr[i]+'.png')+'" /></button>'); //value=":'+arr[i]+':" width=16 height=16
+        }
+        $(":button[id=|smiley]").click(function () {
+                wherever_cursor_is = $('#message').val($('#message').val()+":"+$(this).val()+":");
+        });    
+ }
  
-	// Create a port to BG process
-	var port = chrome.extension.connect();
+function extInit() {
  
-	// Filter out iframes
-	// Send "getSettings" request
-	if (window.top === window) {
-			port.postMessage({ name : "getSettings" });
-	}
- 
-	// Catch incoming messages
-	port.onMessage.addListener(function(event) {
-       
-		// Here comes the settings, save a copy to dataStore var
-        if(event.name == 'setSettings') {
-            dataStore = event.message;
-			alert(event.message);
-		}
-	});
-	
-	
- 	if(dataStore['smiley_gombok'] == 'checked') {
-		smiley_gombok(); 
-	}
-	
+        if(dataStore['smiley_gombok'] == 'checked') {
+                smiley_gombok();
+        }
 }
+ 
+ 
+var dataStore;
+ 
+// Create a port to BG process
+var port = chrome.extension.connect();
+ 
+// Filter out iframes
+// Send "getSettings" request
+if (window.top === window) {
+                port.postMessage({ name : "getSettings" });
+}
+ 
+// Catch incoming messages
+port.onMessage.addListener(function(event) {
+       
+        // Here comes the settings, save a copy to dataStore var
+        if(event.name == 'setSettings') {
+                dataStore = event.message;
+                       
+                $(document).ready(function() {
+                        extInit();
+                });
+        }
+});
