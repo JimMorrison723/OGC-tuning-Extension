ae952a1bb77776d9f9af22158405002d57171fd1
Balazs Szilagyi <bali723@gmail.com>
2011-09-03 17:23:05 +0200

    Fajlok feltoltese

---
+++ js/settings.js
@@ -0,0 +1,20 @@
+function saveOptions() {
+	$('input:checkbox').click(function(){ 
+		localStorage[ $(this).attr('id') ] = $(this).attr('checked'); 
+	});
+}
+
+function loadOptions() {
+
+	$('input:checkbox').each(function() {
+		if(localStorage[ $(this).attr('id') ] == 'checked') {
+			$(this).attr('checked', true);
+		}
+	});
+}
+
+$(document).ready(function() {
+	loadOptions();
+});
+
+
